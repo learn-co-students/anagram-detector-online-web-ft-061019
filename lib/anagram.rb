454442e37068be7c3d0920d7e13ec0345692e015
nbaugh1 <nbaugh1@gmail.com>
2019-06-23 22:09:29 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word) 
+        @word = word
+    end
+
+    def match(word_list)
+        matches = []
+        word_list
+        word_list.each do |arry_word|
+            
+            if (@word.split("").sort == arry_word.split("").sort)
+                matches << arry_word
+            end
+            
+        end
+        matches
+    end
+
+end
